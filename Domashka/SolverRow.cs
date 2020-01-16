1705ae5ea3bff31217c98d28da9729f2ccbb9dca
MaxSimonov <59609806+MaxSimonov@users.noreply.github.com>
2020-01-16 17:20:10 +0500

    Add files via upload

---
+++ Domashka/SolverRow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    class SolverRow
+    {
+
+
+        public byte xId { get; set; }
+
+        public double Koef { get; set; }
+
+       
+    }
+}
---
+++ Domashka/obogrev.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    class obogrev
+    {
+        /// <summary>
+        /// Потребности агрегатов в топливе агрегат 1, т
+        /// </summary>
+        public double Pot_A1 { get; set; }
+
+        /// <summary>
+        /// Потребности агрегатов в топливе агрегат 2, т
+        /// </summary>
+        public double Pot_A2 { get; set; }
+
+        /// <summary>
+        /// Потребности агрегатов в топливе агрегат 3, т
+        /// </summary>
+        public double Pot_A3 { get; set; }
+
+        /// <summary>
+        /// Потребности агрегатов в топливе агрегат 4, т
+        /// </summary>
+        public double Pot_A4 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 1, топливо 1
+        /// </summary>
+        public double Tep_T1_A1 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 2, топливо 1
+        /// </summary>
+        public double Tep_T1_A2 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 3, топливо 1
+        /// </summary>
+        public double Tep_T1_A3 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 4, топливо 1
+        /// </summary>
+        public double Tep_T1_A4 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 1, топливо 2
+        /// </summary>
+        public double Tep_T2_A1 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 2, топливо 2
+        /// </summary>
+        public double Tep_T2_A2 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 3, топливо 2
+        /// </summary>
+        public double Tep_T2_A3 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 4, топливо 2
+        /// </summary>
+        public double Tep_T2_A4 { get; set; }
+
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 1, топливо 3
+        /// </summary>
+        public double Tep_T3_A1 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 2, топливо 3
+        /// </summary>
+        public double Tep_T3_A2 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 3, топливо 3
+        /// </summary>
+        public double Tep_T3_A3 { get; set; }
+
+        /// <summary>
+        /// Теплотворная способность различных топлив агрегат 4, топливо 3
+        /// </summary>
+        public double Tep_T3_A4 { get; set; }
+
+
+        /// <summary>
+        /// Наличие топлива 1
+        /// </summary>
+        public double Zap_T1 { get; set; }
+
+        /// <summary>
+        /// Наличие топлива 2
+        /// </summary>
+        public double Zap_T2 { get; set; }
+
+        /// <summary>
+        /// Наличие топлива 3
+        /// </summary>
+        public double Zap_T3 { get; set; }
+
+       
+
+        /// <summary>
+        /// Потребление топлива 1 агрегатом 1
+        /// </summary>
+        public double Ras_A1_T1 { get; set; }
+
+        /// <summary>
+        /// Потребление топлива 1 агрегатом 2
+        /// </summary>
+        public double Ras_A2_T1 { get; set; }
+
+        /// <summary>
+        /// Потребление топлива 1 агрегатом 3
+        /// </summary>
+        public double Ras_A3_T1 { get; set; }
+
+
+        /// <summary>
+        /// Потребление топлива 1 агрегатом 4
+        /// </summary>
+        public double Ras_A4_T1 { get; set; }
+
+        /// <summary>
+        /// Потребление топлива 2 агрегатом 1
+        /// </summary>
+        public double Ras_A1_T2 { get; set; }
+
+
+        /// <summary>
+        /// Потребление топлива 2 агрегатом 2
+        /// </summary>
+        public double Ras_A2_T2 { get; set; }
+
+
+        /// <summary>
+        /// Потребление топлива 2 агрегатом 3
+        /// </summary>
+        public double Ras_A3_T2 { get; set; }
+
+
+        /// <summary>
+        /// Потребление топлива 2 агрегатом 4
+        /// </summary>
+        public double Ras_A4_T2 { get; set; }
+
+
+        /// <summary>
+        /// Потребление топлива 3 агрегатом 1
+        /// </summary>
+        public double Ras_A1_T3 { get; set; }
+
+
+        /// <summary>
+        /// Потребление топлива 3 агрегатом 2
+        /// </summary>
+        public double Ras_A2_T3 { get; set; }
+
+
+        /// <summary>
+        /// Потребление топлива 3 агрегатом 3
+        /// </summary>
+        public double Ras_A3_T3 { get; set; }
+
+
+        /// <summary>
+        /// Потребление топлива 3 агрегатом 4
+        /// </summary>
+        public double Ras_A4_T3 { get; set; }
+
+
+
+    }
+}
